4b3592ccd543c11de2d2666aa41ac957f000fa53
Kristina Peru <krissy.germanov@gmail.com>
2022-08-04 14:58:53 -0600

    day-8 notes

---
+++ 100-days-of-code/Day-8/day8-notes.py
@@ -0,0 +1,17 @@
+# Review: 
+# Create a function called greet(). 
+# Write 3 print statements inside the function.
+# Call the greet() function and run your code.
+#def greet(name):
+#    print("Hello " + name + "!")
+#    print(f"Welcome to Python day 8, {name}!")
+#    print("You've got this!")
+
+#greet("Zoe")
+
+#def greet_with(name, location):
+#    print(f"Hello {name}!")
+#    print(f"What is it like in {location}?")
+
+#greet_with("Zoe", "Italy")
+#greet_with(location="Wonderland", name="Wendy")
